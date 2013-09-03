e9068d9395e80484a38a78a174850e73d03f58e9
Jonathan <jj@cs.duke.edu>
2013-09-03 08:54:06 -0400

    ProteinPosition.java

---
+++ src/BDAStar/ProteinPosition.java
@@ -0,0 +1,10 @@
+package BDAStar;
+
+public class ProteinPosition extends Position {
+
+    public ProteinPosition (int position) {
+        super(position);
+        // TODO Auto-generated constructor stub
+    }
+
+}
